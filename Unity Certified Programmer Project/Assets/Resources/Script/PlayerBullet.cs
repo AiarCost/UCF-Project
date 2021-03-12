7f010bdfe83f0c48afa8ec2a88cb35b12710bbbc
AiarCost <65986842+AiarCost@users.noreply.github.com>
2021-03-12 11:58:14 -0700

    CH2

---
+++ Unity Certified Programmer Project/Assets/Resources/Script/PlayerBullet.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour,IActorTemplate
+{
+    GameObject actor;
+    int hitPower;
+    int health;
+    int travelSpeed;
+
+    [SerializeField]
+    SOActorModel bulletModel;
+
+    void Awake()
+    {
+        ActorStats(bulletModel);
+    }
+
+    public int SendDamage()
+    {
+        return hitPower;
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(travelSpeed, 0, 0) * Time.deltaTime;
+    }
+    public void TakeDamage(int incomingDamage)
+    {
+        health -= incomingDamage;
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void ActorStats (SOActorModel actorModel)
+    {
+        hitPower = actorModel.hitPower;
+        health = actorModel.health;
+        travelSpeed = actorModel.speed;
+        actor = actorModel.actor;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            if(col.GetComponent<IActorTemplate>() != null)
+            {
+                if(health >= 1)
+                {
+                    health -= col.GetComponent<IActorTemplate>().SendDamage();
+                }
+                if(health <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+    }
+
+    void OnBecomeInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
